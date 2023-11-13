9510fa4f5861367e855a9d896605e267a05fd332
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-11-13 16:32:26 +0100

    wip

--- routes/course_mastering_nova_routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use NovaAdvancedUI\Http\Controllers\Course\WatchVideoController;
-
-Route::get('/videos/{id}', [WatchVideoController::class, 'watch'])->name('video.watch');
